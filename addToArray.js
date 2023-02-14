07a044d015a681390ee83a562ebb3cc43465d990
yonliaoussihabiboulaye@gmail.com <yonliaoussihabiboulaye@gmail.com>
2023-02-14 13:25:06 +0100

    h

---
+++ addToArray.js
@@ -0,0 +1,6 @@
+const myArray = [
+  ["jhone",23],
+  ["cat",2]
+  ];
+  myArray.push(["Cs",1]);
+  console.log(myArray);
---
+++ myStr.js
@@ -0,0 +1,3 @@
+let chaine1 = "This is the start";
+let chainne2 = "This is the end";
+console.log(chaine1 +" " + chainne2);
